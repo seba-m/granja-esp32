ade0fe94f008f5b6d73735facfe54cddfc3b97b2
Sebastian Morgado <sebaah89@gmail.com>
2023-05-20 18:49:15 -0400

    update

--- src/sensors/sensor_temperature.cpp
+++ src/sensors/sensor_temperature.cpp
@@ -1,5 +1,5 @@
 #include <OneWire.h>
-
+#include <DallasTemperature.h>
 #include <settings.h>
 #include <communication/mqtt_manager.cpp>
 
@@ -9,6 +9,8 @@ class TemperatureSensor
         OneWire ds;
         unsigned long timepoint = 0;
         MqttManager &mqttManager;
+        OneWire ourWire;
+        DallasTemperature sensors;
 
     public:
         TemperatureSensor(MqttManager &manager) : mqttManager(manager)
@@ -18,64 +20,30 @@ class TemperatureSensor
         void setup()
         {
             ds = OneWire(temperatureSensorPin);
+            sensors.setOneWire(&ourWire);
+            sensors.begin();
+            sensors.setResolution(12); // Ajusta la resolución del sensor a 12 bits (máxima precisión)
         }
 
-        void loop(unsigned int timeout = 100U)
+        void loop(unsigned int timeout = 500U)
         {
             if (millis() - timepoint > timeout)
             {
                 timepoint = millis();
-                float temperature = getTemp();
-                Serial.print("Temperature= ");
-                Serial.println(temperature, DEC);
+                sensors.requestTemperatures();
+                float temp = sensors.getTempCByIndex(0);
 
-                mqttManager.publish("sensor/temperature", String(temperature));
+                if (temp == -127.00)
+                {
+                    Serial.println("Error: Lectura inválida del sensor");
+                    return;
+                }
+
+                Serial.print("Temperatura = ");
+                Serial.print(temp);
+                Serial.println(" °C");
+
+                mqttManager.publish("sensor/temperature", String(temp));
             }
-        }
-
-    private:
-        float getTemp()
-        {
-            byte data[12];
-            byte addr[8];
-
-            if (!ds.search(addr))
-            {
-                ds.reset_search();
-                return -1000;
-            }
-
-            if (OneWire::crc8(addr, 7) != addr[7])
-            {
-                Serial.println("CRC is not valid!");
-                return -1000;
-            }
-
-            if (addr[0] != 0x10 && addr[0] != 0x28)
-            {
-                Serial.print("Device is not recognized");
-                return -1000;
-            }
-
-            ds.reset();
-            ds.select(addr);
-            ds.write(0x44, 1); // start conversion, with parasite power on at the end
-            byte present = ds.reset();
-            ds.select(addr);
-            ds.write(0xBE); // Read Scratchpad
-
-            for (int i = 0; i < 9; i++)
-            {
-                data[i] = ds.read();
-            }
-
-            ds.reset_search();
-
-            byte MSB = data[1];
-            byte LSB = data[0];
-            float tempRead = ((MSB << 8) | LSB);
-            float temperatureSum = tempRead / 16;
-
-            return temperatureSum;
         }
 };
